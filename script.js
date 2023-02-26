910b4f3e764ba3f162e5b343937d4b52a8b15d10
AlexandreDev <alexandreagradev@gmail.com>
2023-02-25 21:21:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -162,7 +162,7 @@ function createTask(task) {
                 <span ${!task.isCompleted ? "contenteditable" : ""}>${
     task.name
   }</span>
-  <input class="timer" type="time">
+  <input class="timer" type="time" value="">
   <button title="Remove "${task.name}" task" class="remove-task">
   <box-icon name='x' size="28px" color="#FE2E34"></box-icon></box-icon> 
   </button>
